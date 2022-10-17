a0fbe19587713a3dbe6a3bc7dd72583d81e6b3ee
Younes Lahouiti <loller763@gmail.com>
2022-10-17 10:27:56 +0200

    Changement dans le chemin relatif

--- js/pages/index.js
+++ js/pages/index.js
@@ -3,14 +3,27 @@ class IndexApp {
     this.usersDataApi = new PhotographersApi(getApiDataForGitHub());
   }
 
-  static init() {}
+  static init(container) {}
 }
+
+//DOM Elements
+const recipeCardsContainer = document.querySelector(
+  ".main-index__recipes-container"
+);
+
+const searchTagsContainer = document.querySelector(
+  ".main-index__tags-container"
+);
+
+const dropdownMenuOptionsListContainer = document.querySelector(
+  ".dropdown-menu__options-list"
+);
 
 //In order to make the GitHub Pages page work we must use a relative path to the data directory
 function getApiDataForGitHub() {
   const urlAPI = window.location.origin.includes("http://127.0.0.1:5500")
     ? "http://127.0.0.1:5500/P6/workstation/data/photographers.json"
-    : "../data/photographers.json";
+    : "./data/photographers.json";
 
   return urlAPI;
 }
